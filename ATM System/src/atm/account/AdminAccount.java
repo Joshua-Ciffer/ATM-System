d98170cd1d96325064d31ec85bba2d080a2c8b7c
Joshua <Joshua@CYPHER-LAPTOP>
2018-01-18 22:46:22 -0500

    Wrote out admin level methods.

--- ATM System/src/atm/account/AdminAccount.java
+++ ATM System/src/atm/account/AdminAccount.java
@@ -1,4 +1,6 @@
 package src.atm.account;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import src.atm.pin.Pin;
 
 /**
@@ -8,18 +10,71 @@ import src.atm.pin.Pin;
  * This class cannot be inherited.
  * This class inherits Account.
  * 
- * @author Joshua Ciffern
+ * @author Joshua Ciffer
  * @version 01/18/2018
  */
 public final class AdminAccount extends Account {
 
-	public AdminAccount(String accountName, Pin accountPin, String accountHistory) {
-		super(accountName, accountPin, accountHistory);
+	public AdminAccount(String accountName, Pin accountPin) {
+		super(accountName, accountPin, DATE_TIME.format(LocalDateTime.now()) + " - Account Opened\n");
 	}
 
-	public final String listAccounts() {
+	public int createAdminAccount(String accountName, Pin accountPin) throws IllegalArgumentException {
+		int newAccountNumber = new AdminAccount(accountName, accountPin).getAccountNumber();
+		accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Created Admin Account #" + newAccountNumber + ".\n";
+		return newAccountNumber;
+	}
+
+	public int createBankAccount(String accountName, Pin accountPin, BigDecimal accountBalance) throws IllegalArgumentException {
+		int newAccountNumber = new BankAccount(accountName, accountPin, accountBalance).getAccountNumber();
+		accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Created Bank Account #" + newAccountNumber + ".\n";
+		return newAccountNumber;
+	}
+
+	public void editAccountName(int accountNumber, String accountName) throws NullPointerException {
+		if (ACCOUNT_EXISTS(accountNumber)) {
+			GET_ACCOUNT_MAP().get(accountNumber).setAccountName(accountName);
+			accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Changed Name On Account #" + accountNumber + ".\n";
+		}
+	}
+
+	public void editAccountPin(int accountNumber, Pin accountPin) throws NullPointerException, IllegalArgumentException {
+		if (ACCOUNT_EXISTS(accountNumber)) {
+			GET_ACCOUNT_MAP().get(accountNumber).setAccountPin(accountPin);
+			accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Changed Pin On Account #" + accountNumber + ".\n";
+		}
+	}
+
+	public void editAccountHistory(int accountNumber, String accountHistory) throws NullPointerException {
+		if (ACCOUNT_EXISTS(accountNumber)) {
+			GET_ACCOUNT_MAP().get(accountNumber).setAccountHistory(accountHistory);
+			accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Changed History On Account #" + accountNumber + ".\n";
+		}
+	}
+
+	public void editAccountBalance(int accountNumber, BigDecimal accountBalance) throws NullPointerException, IllegalArgumentException {
+		if (ACCOUNT_EXISTS(accountNumber)) {
+			if (GET_ACCOUNT_MAP().get(accountNumber) instanceof BankAccount) {
+				if (BankAccount.IS_POSITIVE_AMOUNT(accountBalance.doubleValue())) {
+					((BankAccount)GET_ACCOUNT_MAP().get(accountNumber)).setAccountBalance(accountBalance);
+					accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Changed Balance On Account #" + accountNumber + ".\n";
+				}
+			} else {
+				throw new IllegalArgumentException("This account is not a bank account.");
+			}
+		}
+	}
+
+	public void deleteAccount(int accountNumber) throws NullPointerException {
+		if (ACCOUNT_EXISTS(accountNumber)) {
+			GET_ACCOUNT_MAP().remove(accountNumber, GET_ACCOUNT_MAP().get(accountNumber));
+			accountHistory = accountHistory + DATE_TIME.format(LocalDateTime.now()) + " - Deleted Account #" + accountNumber + ".\n";
+		}
+	}
+
+	public String getListOfAccounts() {
 		Account[] listOfAccounts = GET_ACCOUNT_MAP().values().toArray(new Account[GET_ACCOUNT_MAP().size()]);	 // HashMap to Collection, to Account[]
-		String accountList = listOfAccounts.length + " accounts created.\n";
+		String accountList = listOfAccounts.length + " in the system.\n";
 		for (Account a : listOfAccounts) {
 			accountList = accountList + a.toString() + "\n";
 		}
