5fb6a3d9bb03bfa232c444e26e78c9c2bce3f737
Joshua <Joshua@CYPHER-LAPTOP>
2018-01-19 09:29:44 -0500

    Started writing command line program

---
+++ ATM System/src/atm/app/ConsoleATM.java
@@ -0,0 +1,173 @@
+package src.atm.app;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.math.BigDecimal;
+import src.atm.account.Account;
+import src.atm.account.BankAccount;
+import src.atm.account.AdminAccount;
+import src.atm.pin.Pin;
+
+public abstract class ConsoleATM {
+
+	private static final Scanner userInput = new Scanner(System.in);
+
+	private static short userResponse;
+
+	private static Account currentAccount;
+
+	public static void main(String[] args) {
+		MAIN_MENU();
+	}
+
+	private static void MAIN_MENU() {
+		do {	// Begin main menu loop.
+			System.out.print("ATM Main Menu\n (1) Login\n (2) Create Account\n (3) Exit\nEnter an optioin: ");
+			try {
+				userResponse = userInput.nextShort();
+			} catch (InputMismatchException e) {
+				System.out.println("\nPlease enter one of the given options.\n");
+				userInput.next();
+				continue;
+			}
+			switch (userResponse) {		// Begin userResponse switch.
+				case 1: {	// Login.
+					LOGIN();
+					break;
+				}
+				case 2: {	// Create account.
+					CREATE_ACCOUNT();
+					break;
+				}
+				case 3: {	// Exit.
+					EXIT();
+					break;
+				}
+				default: {	 // Error.
+					System.out.println("\nPlease enter one of the given options.\n");
+					continue;
+				}
+			}	// End userResponse switch.
+		} while (true);		// End main menu loop.
+	}
+
+	private static void LOGIN() {
+		int accountNumber;
+		String accountPin;
+		System.out.print("\n");
+		do {	// Begin account number loop.
+			System.out.print("Enter your account number: #");
+			try {
+				accountNumber = userInput.nextInt();
+				Account.ACCOUNT_EXISTS(accountNumber);
+			} catch (InputMismatchException e) {
+				System.out.println("\nPlease enter your account number.\n");
+				userInput.next();
+				continue;
+			} catch (NullPointerException e) {
+				System.out.println("\n" + e.getMessage() + "\n");
+				break;
+			}
+			do {	// Begin account PIN loop.
+				System.out.print("Enter your account PIN: ");
+				try {
+					accountPin = userInput.next();
+					Pin.IS_CORRECT_PIN(accountNumber, accountPin);
+				} catch (InputMismatchException e) {
+					System.out.println("\nPlease enter your account number.\n");
+					userInput.next();
+					continue;
+				} catch (IllegalArgumentException e) {
+					System.out.println("\n" + e.getMessage() + "\n");
+					break;
+				}
+				currentAccount = Account.GET_ACCOUNT(accountNumber, accountPin);
+				if (currentAccount instanceof BankAccount) {
+					System.out.println(((BankAccount)currentAccount).toString());
+					BANK_ACCOUNT_MENU();
+				} else if (currentAccount instanceof AdminAccount) {
+					ADMIN_ACCOUNT_MENU();
+				}
+				break;
+			} while (true);		// End account PIN loop.
+			break;
+		} while (true);	    // End account number loop.
+	}
+
+	private static void CREATE_ACCOUNT() {
+		String accountName, accountPin, confirmPin;
+		double accountBalance;
+		userInput.nextLine();
+		System.out.print("\n");
+		do {	// Begin account name loop.
+			System.out.print("Enter your name: ");
+			try {
+				accountName = userInput.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println("\nPlease enter your name.\n");
+				userInput.next();
+				continue;
+			}
+			do {	// Begin account PIN loop.
+				System.out.print("Create an account PIN: ");
+				try {
+					accountPin = userInput.next();
+					Pin.IS_VALID_PIN(accountPin);
+				} catch (InputMismatchException e) {
+					System.out.println("\nPlease create an account PIN.\n");
+					userInput.next();
+					continue;
+				} catch (IllegalArgumentException e) {
+					System.out.println("\n" + e.getMessage() + "\n");
+					continue;
+				}
+				do {	// Begin confirm PIN loop.
+					System.out.print("Confirm your account PIN: ");
+					try {
+						confirmPin = userInput.next();
+						Pin.PINs_MATCH(accountPin, confirmPin);
+					} catch (InputMismatchException e) {
+						System.out.println("\nPlease confirm your account PIN.\n");
+						userInput.next();
+						continue;
+					} catch (IllegalArgumentException e) {
+						System.out.println("\n" + e.getMessage() + "\n");
+						continue;
+					}
+					do {	// Begin account balance loop.
+						System.out.print("Enter your starting balance: $");
+						try {
+							accountBalance = userInput.nextDouble();
+							BankAccount.IS_POSITIVE_AMOUNT(accountBalance);
+						} catch (InputMismatchException e) {
+							System.out.println("\nPlease enter your account's starting balance.\n");
+							userInput.next();
+							continue;
+						} catch (IllegalArgumentException e) {
+							System.out.println("\n" + e.getMessage() + "\n");
+							continue;
+						}
+						System.out.println("\nAccount created. Your account number is #"
+								+ new BankAccount(accountName, new Pin(accountPin, confirmPin), new BigDecimal(accountBalance)).getAccountNumber() + ".\n");
+						break;
+					} while (true);		// End account balance loop.
+					break;
+				} while (true);		// End confirm PIN loop.
+				break;
+			} while (true);		// End account PIN loop.
+			break;
+		} while (true);		// End account name loop.
+	}
+
+	private static void EXIT() {
+		System.exit(0);
+	}
+
+	private static void BANK_ACCOUNT_MENU() {
+
+	}
+
+	private static void ADMIN_ACCOUNT_MENU() {
+
+	}
+
+}
---
+++ ATM System/src/atm/app/GraphicalATM.java
@@ -0,0 +1,9 @@
+package src.atm.app;
+
+public abstract class GraphicalATM {
+
+	public static void main(String[] args) {
+
+	}
+
+}
