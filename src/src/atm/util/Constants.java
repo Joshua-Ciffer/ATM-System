80da24e6fb30d30f5c8edddf6b7c7d069d28b0d8
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-04-06 23:43:45 -0400

    Created constants file.

---
+++ src/src/atm/util/Constants.java
@@ -0,0 +1,16 @@
+package src.atm.util;
+
+/**
+ * This interface contains project wide values and objects.
+ *
+ * @author Joshua
+ * @version 04/06/2018
+ */
+interface Constants {
+
+	/**
+	 * The exit code used when invalid command line arguments are passed to the main entry point.
+	 */
+	int INVALID_COMMAND_ARGS_CODE = 1;
+
+}
