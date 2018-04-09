53a9256e7864643d106ef39647d163815cdcb52e
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-04-09 00:17:35 -0400

    Nuked command line code.

--- src/main/java/src/atm/Main.java
+++ src/main/java/src/atm/Main.java
@@ -5,12 +5,10 @@ import src.atm.gui.GUI;
 import static src.atm.util.Constants.INVALID_COMMAND_ARGS_CODE;
 
 /**
- * This class contains the main entry point for the ATM System program. By default the main() method will create a GUI frame to interface with the
- * program. This class also contains methods to provide a command line implementation of the program if the command line argument "--no-gui" is
- * passed.
+ * This class contains the main entry point for the ATM System program.
  * 
  * @author Joshua Ciffer
- * @version 04/08/2018
+ * @version 04/09/2018
  */
 public final class Main {
 
